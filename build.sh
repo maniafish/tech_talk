00d78ed3d716eaa0203607fb69736b547d668f73
maniafish <mania_fish@163.com>
2019-01-25 16:07:41 +0800

    build

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ VER=$1
 /bin/rm -rf node_modules/
 gitbook install
 gitbook build
-rm -rf ../tech_talk_pages/*
+/bin/rm -rf ../tech_talk_pages/*
 cp -rf _book/* ../tech_talk_pages/
 cp -rf node_modules/ ../tech_talk_pages/node_modules
 cd ../tech_talk_pages
-git commit -a -m "v${VER}"
+git add .
+git commit -m "v${VER}"
 git push origin gh-pages
